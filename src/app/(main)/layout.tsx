98751bf815fc543ac9b07d4d87827b787dd0d909
Amir <amir224030@bue.edu.eg>
2025-06-21 20:41:16 +0300

    ui fixes

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -10,15 +10,15 @@ type Props = {
 const Layout = ({ children }: Props) => {
   return (
     <div className='flex justify-center'>
-      <div className='flex w-full max-w-7xl min-w-fit'>
+      <div className='flex w-full max-w-7xl justify-center'>
         {/* Navbar (Left Sidebar) */}
         <Navbar />
 
         {/* Main Content */}
-        <main className='w-full max-w-[590px] flex-1 sm:border-x'>{children}</main>
+        <main className='w-full max-w-[590px] sm:border-x'>{children}</main>
 
         {/* Sidebar (Right) */}
-        <div className='sticky top-0 hidden shrink-0 self-start p-3 pt-0 lg:block lg:w-[320px] xl:w-[410px]'>
+        <div className='sticky top-0 hidden shrink-0 self-start p-3 pt-0 lg:block lg:w-[330px] xl:w-[360px]'>
           <Sidebar />
         </div>
       </div>
